f910601e5b9897b71cc0bfa5ea399003eb04b398
Brett Brading <bbbrading@gmail.com>
2017-03-06 10:38:30 -0500

    added add function to calculator to allow for adding multiple numbers together.

--- calculator.js
+++ calculator.js
@@ -1,24 +1,63 @@
 $(document).ready(function(){
   var firstNum = "";
-  var secondNum = "";
+  var secondNum = "0";
+  var currentNum = "";
+  var answer = "";
+
   $(".button").on("click", clickEvent)
-  //$("#.add").on("click", add)
+  $("#add").on("click", add)
+  $("")
+  $("#equals").on("click", displayAnswer)
 
   function clickEvent(e){
-    if (firstNum == ""){
-      firstNum += e.currentTarget.id
-      $("#screen").html(firstNum)
+    if (firstNum === ""){
+      currentNum += e.currentTarget.id
+      $("#screen").html(currentNum)
     }
     else {
       secondNum += e.currentTarget.id
       $("#screen").html(secondNum)
     }
-
   }
 
-  /*function add(){
-    var sum = parseInt(firstNum, 10) + parseInt(secondNum, 10)
-    $("screen").html(sum)
-  }*/
+  function add(){
+    if(firstNum === ""){
+      firstNum = parseInt(currentNum, 10);
+      answer = (parseInt(firstNum, 10) + parseInt(secondNum, 10)) || parseInt(firstNum, 10)
+      $("#screen").html(answer)
+      firstNum = answer
+      secondNum = 0;
+    }
+    else{
+    answer = (parseInt(firstNum, 10) + parseInt(secondNum, 10)) || parseInt(firstNum, 10)
+    $("#screen").html(answer)
+    firstNum = answer
+    secondNum = 0;
+    }
+  }
+
+  function subtract(){
+    if(firstNum === ""){
+      firstNum = parseInt(currentNum, 10);
+      answer = (parseInt(firstNum, 10) - parseInt(secondNum, 10)) || parseInt(firstNum, 10)
+      $("#screen").html(answer)
+      firstNum = answer
+      secondNum = 0;
+    }
+    else{
+    answer = (parseInt(firstNum, 10) - parseInt(secondNum, 10)) || parseInt(firstNum, 10)
+    $("#screen").html(answer)
+    firstNum = answer
+    secondNum = 0;
+    }
+  }
+
+  function displayAnswer(){
+    $("#screen").html(answer)
+  }
+
+  function clear(){
+
+  }
 
 })
